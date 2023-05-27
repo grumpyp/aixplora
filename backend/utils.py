c4324803533cdd108fc2f8aef788e657ce267ef1
grumpyp <info@gerryontour.de>
2023-05-27 13:51:18 +0100

    refactor indexation to store metadata of file: txt_loader

--- backend/utils.py
+++ backend/utils.py
@@ -8,7 +8,7 @@ import openai
 
 FILE_HANDLERS = {
     ".pdf": lambda file: load_pdf(file.file, filename=file.filename),
-    ".txt": lambda file: load_txt(file.file, filename=file.filename),
+    ".txt": lambda file: load_txt(file.file, filename=file.filename, file_meta=file),
     ".mp3": lambda file: Whisperexporter().whisper_to_text(file.file, filename=file.filename),
     ".mp4": lambda file: Whisperexporter().whisper_to_text(file.file, filename=file.filename),
     ".mpeg": lambda file: Whisperexporter().whisper_to_text(file.file, filename=file.filename),
