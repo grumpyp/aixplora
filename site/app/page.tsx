265d0e8d8f80513147d41d129f42dc21fbf6ecd4
duckduckcodes <0x07d2@proton.me>
2023-05-28 22:10:15 +0200

    added metadata

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -18,19 +18,3 @@ export default function Home() {
   );
 }
 
-// <div
-// href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-// className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-// target="_blank"
-// rel="noopener noreferrer"
-// >
-// <h2 className={`mb-3 text-2xl font-semibold`}>
-//   Deploy{' '}
-//   <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-//     -&gt;
-//   </span>
-// </h2>
-// <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-//   Instantly deploy your Next.js site to a shareable URL with Vercel.
-// </p>
-// </div>
