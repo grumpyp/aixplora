3181d1af0cfcff60fe31c41ca2c9a49efba07574
Sourav Das (Dipta) <dip9623@outlook.com>
2023-06-04 20:42:18 +0600

    chore: simplify workflow

--- frontend/docker-fe-command.sh
+++ frontend/docker-fe-command.sh
@@ -1,11 +1,18 @@
-echo "installing packages..."
-npm install
+#!/bin/bash
 
-echo "setting permissions..."
-chown -R node /usr/src/app
-chown root /usr/src/app/node_modules/electron/dist/chrome-sandbox
-chmod 4755 /usr/src/app/node_modules/electron/dist/chrome-sandbox
+if [ -d "node_modules" ]; then
+  echo "node_modules directory exists. Skipping npm install."
+  echo "If your dependencies have changed, please remove node_modules folder and run the container"
+else
+  echo "node_modules directory not found. Running npm install."
+  npm install
+
+  echo "setting permissions..."
+  chown -R node /usr/src/app
+  chown root /usr/src/app/node_modules/electron/dist/chrome-sandbox
+  chmod 4755 /usr/src/app/node_modules/electron/dist/chrome-sandbox
+fi
+
 
 echo "starting app..."
-ls -ltrs
 su - node -c "cd /usr/src/app/ && npm start"
