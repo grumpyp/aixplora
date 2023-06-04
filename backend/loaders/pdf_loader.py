1e67bbb099f66947a61c70540ed439b3b9f3ad3d
Patrick Gerard <75830792+grumpyp@users.noreply.github.com>
2023-06-04 08:58:16 +0100

    add new chunk indexing strategy (#55)
    * WIP: drafting new indexing
    
    * wip improve chunking 1000 chars
    
    * fix qdrant locking bug
    
    * delete dataclasses
    
    * delete comments and debug
    
    * implement page metadata for pdf loader
    
    * add Textsplitter tests
    
    * delete prints from tests
    
    * add nltk to requirements
    
    * delete mui/types from package.json
    
    * del comments in test
    
    * add testing github action
    
    * add synchronize to action
    
    * pytest
    
    * nltk package
    
    * fix requirements install action
    
    * backend path

--- backend/loaders/pdf_loader.py
+++ backend/loaders/pdf_loader.py
@@ -9,12 +9,13 @@ def load_pdf(file: bytes, filename: str, file_meta: UploadFile):
 
     # write files to misc folder
     misc_dir = os.path.join(os.getcwd(), "misc")
-    with open(f"{misc_dir}/{filename}.txt", "w") as f:
-        for i in range(number_of_pages):
+
+    for i in range(number_of_pages):
+        with open(f"{misc_dir}/{filename}{i}.txt", "w") as f:
             page = reader.pages[i]
             text = page.extract_text().strip()
             f.write(text.replace("\n", " "))
 
         f.close()
 
-    return f"{misc_dir}/{filename}.txt", file_meta
+    return [f"{misc_dir}/{filename}{i}.txt" for i in range(number_of_pages)], file_meta
