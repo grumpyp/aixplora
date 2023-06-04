b1485c06a73680d60354f645d6cdad31f1c60f9f
grumpyp <info@gerryontour.de>
2023-06-04 08:34:44 +0100

    del comments in test

--- backend/tests/test_text_splitter.py
+++ backend/tests/test_text_splitter.py
@@ -5,15 +5,13 @@ from nltk.corpus import words
 from embeddings.text_splitter import TextSplitter
 from embeddings.basesplit import ContextTypes
 
+
 def generate_random_sentence():
-    # Set up NLTK
     nltk.download("punkt")
     nltk.download("words")
 
-    # Get a list of English words
     word_list = words.words()
 
-    # Generate a random sentence
     sentence = []
     i = 0
     while len(sentence) < random.randint(5, 15):
@@ -28,7 +26,7 @@ def generate_random_sentence():
 
 
 def test_text_splitter():
-    # Generate a random text with between 8000 and 2200 characters using random sentences
+    # Generate a random text
     text = " ".join([generate_random_sentence() for _ in range(random.randint(8, 100))])
     chunks = TextSplitter(text=text, context_type=ContextTypes.TEXT).chunk_document()
     assert math.floor(len(text) / 1000) == len(chunks) or math.ceil(len(text) / 1000) == len(chunks)
