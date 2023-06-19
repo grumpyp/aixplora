264e674d2c2648861eab3efd88238ac9fdff02af
grumpyp <info@gerryontour.de>
2023-06-19 21:14:12 +0100

    feat: api-key management

--- frontend/src/renderer/pages/config/Config.tsx
+++ frontend/src/renderer/pages/config/Config.tsx
@@ -1,61 +1,75 @@
-import { useForm } from '@mantine/form';
-import { TextInput, Button, Box } from '@mantine/core';
+import {useForm} from '@mantine/form';
+import {TextInput, Button, Box, Select} from '@mantine/core';
 import axios from 'axios';
 import config from '../../config.js';
 
-function saveConfig(OPENAI_API_KEY) {
-  const payload = {
-    apiKey: OPENAI_API_KEY
-  };
-  return axios.post(`${config.REACT_APP_BACKEND_URL}/config`, payload)
-    .then((response) => {
-      const fetchedConfig = response.data;
+function saveConfig(OPENAI_API_KEY: string, model: string) {
+    const payload = {
+        apiKey: OPENAI_API_KEY,
+        model: model
+    };
+    return axios.post(`${config.REACT_APP_BACKEND_URL}/config`, payload)
+        .then((response) => {
+            const fetchedConfig = response.data;
 
-      if (Object.keys(fetchedConfig).length === 0) {
-        return false;
-      }
+            if (Object.keys(fetchedConfig).length === 0) {
+                return false;
+            }
 
-      // The fetched config is not an empty object, save it and return true
-      localStorage.setItem('config', JSON.stringify(fetchedConfig));
-      console.log(fetchedConfig);
-      window.location.reload();
+            // The fetched config is not an empty object, save it and return true
+            localStorage.setItem('config', JSON.stringify(fetchedConfig));
+            console.log(fetchedConfig);
+            window.location.reload();
 
-      return true;
-    })
-    .catch((error) => {
-      console.log('Error fetching config:', error);
-      return false;
-    });
+            return true;
+        })
+        .catch((error) => {
+            console.log('Error fetching config:', error);
+            return false;
+        });
 }
 
 function Config() {
-  const form = useForm({
-    initialValues: { OPENAI_API_KEY: ''},
+    const form = useForm({
+        initialValues: {OPENAI_API_KEY: ''},
 
-    validate: {
-      OPENAI_API_KEY: (value) => (value.startsWith("sk-") ? null : "Invalid API Key"),
-    },
-  });
+        validate: {
+            OPENAI_API_KEY: (value) => (value.startsWith("sk-") ? null : "Invalid API Key"),
+            model: (value) => (value ? null : "Choose a model")
+        },
+    });
 
-  const handleSuccess = (values) => {
-    console.log(values);
-    saveConfig(values.OPENAI_API_KEY);
-  }
+    const handleSuccess = (values) => {
+        console.log(values);
+        saveConfig(values.OPENAI_API_KEY, values.model);
+    }
 
-  const handleFail = (errors) => {
-    console.log(errors);
-  }
+    const handleFail = (errors) => {
+        console.log(errors);
+    }
 
-  return (
-    <Box maw={320} mx="auto">
-      <form onSubmit={form.onSubmit(handleSuccess, handleFail)}>
-        <TextInput mt="sm" label="OPENAI API Key" placeholder="OPENAI API Key" {...form.getInputProps('OPENAI_API_KEY')} />
-        <Button type="submit" mt="sm">
-          Save
-        </Button>
-      </form>
-    </Box>
-  );
+    return (
+        <Box maw={320} mx="auto">
+            <form onSubmit={form.onSubmit(handleSuccess, handleFail)}>
+                <TextInput mt="sm" label="OPENAI API Key"
+                           placeholder="OPENAI API Key" {...form.getInputProps('OPENAI_API_KEY')} />
+                <Select
+                    label="OPENAI Model"
+                    placeholder="gpt3.5-turbo"
+                    {...form.getInputProps('model')}
+                    data={[
+                        {value: 'gpt-3.5-turbo', label: 'gpt-3.5-turbo'},
+                        {value: 'gpt-3.5-turbo-16k', label: 'gpt-3.5-turbo-16k'},
+                        {value: 'gpt-4', label: 'gpt-4'},
+                        {value: 'gpt-4-32k', label: 'gpt-4-32k'},
+                    ]}
+                />
+                <Button type="submit" mt="sm">
+                    Save
+                </Button>
+            </form>
+        </Box>
+    );
 }
 
 export default Config;
