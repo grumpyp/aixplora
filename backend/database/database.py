52269de7aad4b2c1b37d6ce0b3d96644d5b5d676
Sadaf-A <sadaf20043006@gmail.com>
2023-08-04 09:43:16 +0530

    using factory in database.py

--- backend/database/database.py
+++ backend/database/database.py
@@ -5,18 +5,20 @@ from database.models.base import Base
 
 SQLITE_DATABASE_URL = "sqlite:///backend/database/aixplora.db"
 
-class Database:
+class DatabaseFactory:
 
     def __init__(self):
         self.engine = create_engine(SQLITE_DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
         Base.metadata.create_all(self.engine)
-        self.sessionmaker = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
 
-    def get_session(self):
-        return self.sessionmaker()
+    def create_session(self):
+        return sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+
+def get_database():
+    return DatabaseFactory().create_session()
 
 if __name__ == "__main__":
-    db = Database().get_session()
+    db = get_database()
     result = db.execute(text("SELECT * FROM notes"))
     for row in result:
         print(row)
