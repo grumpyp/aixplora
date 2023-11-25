6d9055e1d4b76d43ebb55dae1da6beea06684a46
Md Abid Hussain <101964499+md-abid-hussain@users.noreply.github.com>
2023-11-25 05:46:37 +0530

    fix the responsiveness of the page (#137)

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -2,13 +2,13 @@
 
 import Hero from "@/pages/components/Hero";
 import Navbar from "@/pages/components/Navbar";
- 
+
 
 export default function Home() {
   return (
-    <main className="relative">
-      <Navbar />
-      <Hero />
+    <main className="min-h-screen overflow-hidden p-6 sm:px-16 md:px-20 lg:px-24">
+        <Navbar />
+        <Hero />
     </main>
   );
 }
