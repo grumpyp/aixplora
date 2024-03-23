16be9da3abcfa960b5b7fcb4fc91d982c6e57b8f
grumpyp <info@gerryontour.de>
2024-03-23 16:36:03 +0100

    fs module not found

--- site/next.config.js
+++ site/next.config.js
@@ -4,3 +4,27 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+
+  // Can be safely removed in newer versions of Next.js
+  future: {
+
+    // by default, if you customize webpack config, they switch back to version 4.
+    // Looks like backward compatibility approach.
+    webpack5: true,   
+  },
+
+  webpack(config) {
+    config.resolve.fallback = {
+
+      // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped.
+      ...config.resolve.fallback,  
+
+      fs: false, // the solution
+    };
+    
+    return config;
+  },
+};
